b39806a9ff4e3e2be8bdd826df8c12fbb1811890
stas <s.kutyepov@gmail.com>
2018-07-21 19:14:09 -0400

    image grid

--- app/src/main/java/com/example/stas/movies/view/MovieListFragment.java
+++ app/src/main/java/com/example/stas/movies/view/MovieListFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,8 +19,6 @@ import com.example.stas.movies.viewmodel.MovieListViewModel;
 
 import java.util.List;
 
-//import com.example.test.mvvmsampleapp.databinding.FragmentProjectListBinding;
-
 public class MovieListFragment extends Fragment {
     public static final String TAG = "MovieListFragment";
     private ImageAdapter imageAdapter;
@@ -28,7 +27,7 @@ public class MovieListFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        imageAdapter = new ImageAdapter();
+        imageAdapter = new ImageAdapter(getActivity());
         final MovieListViewModel viewModel =
                 ViewModelProviders.of(this).get(MovieListViewModel.class);
 
@@ -39,6 +38,7 @@ public class MovieListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
         recyclerView.setAdapter(imageAdapter);
     }
 
--- app/src/main/java/com/example/stas/movies/view/adapter/ImageAdapter.java
+++ app/src/main/java/com/example/stas/movies/view/adapter/ImageAdapter.java
@@ -1,20 +1,27 @@
 package com.example.stas.movies.view.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.example.stas.movies.R;
 import com.example.stas.movies.model.Movie;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
     private List<Movie> movieList = new ArrayList<>();
+    private Context mContext;
+
+    public ImageAdapter (Context context) {
+        mContext = context;
+    }
 
     @NonNull
     @Override
@@ -41,15 +48,18 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHol
 
     class ImageViewHolder extends RecyclerView.ViewHolder {
 
-        private final TextView title;
+        private final ImageView imageView;
 
         public ImageViewHolder(@NonNull View itemView) {
             super(itemView);
-            title = itemView.findViewById(R.id.movie_title);
+            imageView = itemView.findViewById(R.id.movie_poster);
         }
 
         public void bind(Movie movie) {
-            title.setText(movie.getTitle());
+            Picasso
+                    .with(mContext)
+                    .load("https://image.tmdb.org/t/p/w185/" + movie.getPartialImagePath())
+                    .into(imageView);
         }
     }
 }
